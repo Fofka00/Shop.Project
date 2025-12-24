24d14a12bc40d0ec49d3c3931f67692f31c5f091
Fofka00 <47884870+Fofka00@users.noreply.github.com>
2025-12-24 05:33:08 +0300

    1

---
+++ first-react-app/Readme.txt
@@ -0,0 +1,33 @@
+Запук backend (Express + API)
+В папке корня проекта:
+
+Установи зависимости:
+npm install
+
+запуск:
+nodemon start.ts
+Сервер должен стартовать на http://localhost:3000.
+
+
+
+Запуск админку (Shop.Admin)
+В папке
+Shop.Admin:
+
+Установи зависимости:
+npm install
+
+запуск:
+node app.js
+Админка будет доступна на http://localhost:4000/admin.
+
+
+Запуск React‑клиента (Shop.Client)
+В папке Shop.Client:
+
+Установи зависимости:
+npm install
+
+запуск:
+npm start
+Сервер должен стартовать на http://localhost:3001.
--- first-react-app/index.ts
+++ first-react-app/index.ts
@@ -1,22 +1,27 @@
 import express, { Express } from "express";
+import path from "path";
 import { Connection } from "mysql2/promise";
 import { initDataBase } from "./Server/services/db";
 import { initServer } from "./Server/services/server";
 import { productsRouter } from "./src/api/products-api";
+import { commentsRouter } from "./src/api/comments-api";
 
 let app: Express;
 export let connection: Connection;
-export type CommentCreatePayload = {};
 
 export default async (): Promise<Express> => {
   app = initServer();
   connection = await initDataBase();
 
-
   app.use("/api/products", productsRouter);
+  app.use("/api/comments", commentsRouter);
 
-  app.use("/", (_, res) => {
-    res.send("React App");
+  const clientBuildPath = path.join(__dirname, "shop-client", "build");
+
+  app.use(express.static(clientBuildPath));
+
+  app.get("/", (_, res) => {
+    res.sendFile(path.join(clientBuildPath, "index.html"));
   });
 
   return app;
